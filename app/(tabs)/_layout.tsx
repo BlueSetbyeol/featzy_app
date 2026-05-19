e8f75dc968c5955262126cb2d9bb6143166defcd
BlueSetbyeol <setbyeol.c@gmail.com>
2026-05-19 14:52:09 +0200

    Feat #0: First commit after following Tutorial

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,35 +1,102 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+import { Image } from "expo-image";
+import { Tabs } from "expo-router";
+import { StyleSheet } from "react-native";
 
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
+const Home = require("@/assets/images/icon/home.svg");
+const HomeSelected = require("@/assets/images/icon/home_selected.svg");
+const Map = require("@/assets/images/icon/map.svg");
+const MapSelected = require("@/assets/images/icon/map_selected.svg");
+const Resa = require("@/assets/images/icon/reservation.svg");
+const ResaSelected = require("@/assets/images/icon/reservation_selected.svg");
+const Profil = require("@/assets/images/icon/profil.svg");
+const ProfilSelected = require("@/assets/images/icon/profil_selected.svg");
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        tabBarActiveTintColor: "#ffffff",
         headerShown: false,
-        tabBarButton: HapticTab,
-      }}>
+        // headerStyle: {
+        //   backgroundColor: "#860909",
+        // },
+        // headerShadowVisible: false,
+        // headerTintColor: "#fff",
+        tabBarStyle: {
+          backgroundColor: "#860909",
+          height: 80,
+          paddingTop: 5,
+        },
+        tabBarIconStyle: {
+          flex: 1,
+          alignItems: "center",
+          justifyContent: "center",
+        },
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          title: "Accueil",
+          tabBarShowLabel: false,
+          tabBarIcon: ({ color, focused }) => (
+            <Image
+              source={focused ? Home : HomeSelected}
+              style={styles.image}
+              contentFit="contain"
+            />
+          ),
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="map"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: "Map",
+          tabBarShowLabel: false,
+          tabBarIcon: ({ color, focused }) => (
+            <Image
+              source={focused ? Map : MapSelected}
+              style={styles.image}
+              contentFit="contain"
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="reservation"
+        options={{
+          title: "Réservation",
+          tabBarShowLabel: false,
+          tabBarIcon: ({ color, focused }) => (
+            <Image
+              source={focused ? Resa : ResaSelected}
+              style={styles.image}
+              contentFit="contain"
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profil"
+        options={{
+          title: "Profil",
+          tabBarShowLabel: false,
+          tabBarIcon: ({ color, focused }) => (
+            <Image
+              source={focused ? Profil : ProfilSelected}
+              style={styles.image}
+              contentFit="contain"
+            />
+          ),
         }}
       />
     </Tabs>
   );
 }
+
+const styles = StyleSheet.create({
+  image: {
+    width: 30,
+    height: 30,
+  },
+});
